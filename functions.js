a078664fe1e80f645bf2d55702626ffe49c01a95
David1991Grig <davoaleks1991@gmail.com>
2025-05-01 12:53:40 +0300

    second

--- functions.js
+++ functions.js
@@ -1,28 +1,14 @@
+import {parse} from 'csv-parse/sync';
 
 export function changeDate(arr) {
-    const headers = arr[0].split(',')
-    const newDate = []
-    const newArr = arr.map(a => removeCommasInsideQuotes(a))
-    for (let i = 1; i < newArr.length; i++) {
-        const values = newArr[i].trim().split(',');
+    const csvText = arr.join('\n');
+    return parse(csvText, {
+        columns: true,
+        skip_empty_lines: true,
+        trim: true
+    });
+}
 
-        newDate.push(createPassenger( headers, values));
-    }
-    return newDate
-}
-function createPassenger(headers, values) {
-    const obj = {};
-    headers.forEach((key, index) => {
-        obj[key] = values[index];
-    });
-    return obj;
-}
-function removeCommasInsideQuotes(line) {
-    return line.replace(/"([^"]*)"/g, (match, insideQuotes) => {
-        const cleaned = insideQuotes.replace(/,/g, '');
-        return `"${cleaned}"`;
-    });
-}
 
 export function calcTotalSur(arr) {
     const survived = arr.filter(f => f.Survived === '1')
