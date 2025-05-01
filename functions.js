37dd5f1b19073ee578d4ff48bd2da5ae4c19da80
David1991Grig <davoaleks1991@gmail.com>
2025-05-01 12:50:24 +0300

    first

---
+++ functions.js
@@ -0,0 +1,34 @@
+
+export function changeDate(arr) {
+    const headers = arr[0].split(',')
+    const newDate = []
+    const newArr = arr.map(a => removeCommasInsideQuotes(a))
+    for (let i = 1; i < newArr.length; i++) {
+        const values = newArr[i].trim().split(',');
+
+        newDate.push(createPassenger( headers, values));
+    }
+    return newDate
+}
+function createPassenger(headers, values) {
+    const obj = {};
+    headers.forEach((key, index) => {
+        obj[key] = values[index];
+    });
+    return obj;
+}
+function removeCommasInsideQuotes(line) {
+    return line.replace(/"([^"]*)"/g, (match, insideQuotes) => {
+        const cleaned = insideQuotes.replace(/,/g, '');
+        return `"${cleaned}"`;
+    });
+}
+
+export function calcTotalSur(arr) {
+    const survived = arr.filter(f => f.Survived === '1')
+    const nonSurvived = arr.filter(f => f.Survived === '0')
+    return {
+        survived: survived,
+        nonSurvived: nonSurvived
+    }
+}
---
+++ statistics.js
@@ -0,0 +1,28 @@
+import fs from 'node:fs';
+import {changeDate} from "./functions.js";
+import {calcAvrFarInClasses, calcTotalFares, calcTotalSurQuan, calculate} from "./Homework_func.js";
+
+
+
+fs.readFile('./train.csv', 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        const lines = data.split('\n').map(line => line.trim());
+        const passenger = changeDate(lines);
+//                            1. Calculate total fares;
+        console.log(`Calculate total fares;\n${JSON.stringify(calcTotalFares(passenger))}\n`);
+//                2.Calculate average fare for 1,2,3 classes of travel;
+        console.log(`Calculate average fare for 1 classes of travel;\n${JSON.stringify(calcAvrFarInClasses(passenger, 1))}\n`)
+        console.log(`Calculate average fare for 2 classes of travel;\n${JSON.stringify(calcAvrFarInClasses(passenger, 2))}\n`)
+        console.log(`Calculate average fare for 3 classes of travel;\n${JSON.stringify(calcAvrFarInClasses(passenger, 3))}\n`)
+//               3.Calculate total quantity of survived and non survived passengers;
+        console.log(`Calculate total quantity of survived and non survived passengers;nn\n${JSON.stringify(calcTotalSurQuan(passenger))}\n`)
+//   4.Calculate total quantity of survived and non survived men, women and children(under 18 years old);
+        console.log(`Calculate total quantity of survived and non survived men, women and children(under 18 years old);\n${JSON.stringify(calculate(passenger))}\n`)
+
+    }
+})
+
+
+// node ./statistics.js
